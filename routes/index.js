77194fdbbd5cff6cdcac4743e6807283c5e223e3
kaija <kaija.chang@gmail.com>
2015-09-18 20:05:26 +0800

    Add explore api show HTTP request information

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/explore', function(req, res, next) {
+  var resp = {};
+  resp['request-headers'] = req.headers;
+  resp['request-params'] = req.params;
+  resp['request-body'] = req.body;
+  resp['request-query'] = req.query;
+  info = resp['request-info'] = {}
+  info['address'] = req.ip;
+  info['proxys'] = req.ips;
+  info['path'] = req.originalUrl;
+  info['method'] = req.method;
+  info['cookie'] = req.signedCookies;
+  info['protocol'] = req.protocol;
+  info['secure'] = req.secure?'tls':'ssl';
+  info['recv-time'] = req._startTime;
+  console.log(req.ServerResponse);
+  if (req.query.pretty) {
+      resp = "<pre id=\'json\'>\n" + JSON.stringify(resp, null, 4) + "\n</pre>";
+  }
+  console.log(resp);
+  res.send(resp);
+});
+
 module.exports = router;
